b6b4b9370124a4f99c47696501b871c681bfe657
NirdeshPatel-hub <nirdeshpatel280@gmail.com>
2025-09-21 15:11:58 +0530

    Add files via upload

---
+++ mindcare_app.py
@@ -0,0 +1,7 @@
+﻿# -*- coding: utf-8 -*-
+import streamlit as st
+import google.generativeai as genai
+from datetime import datetime
+import time
+
+# [Rest of the code from above - copy the entire code here]
